7dd38a4fcd604091334cb35ce32bfc9bfb9ccc23
Cleber <cleber@4beerdev.com.br>
2023-11-13 10:07:44 -0300

    13-11Ajustes

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 
 //função carousel
 
-const carouselContainer = document.querySelector('.carousel-container');
+// const carouselContainer = document.querySelector('.carousel-container');
 const slides = document.querySelectorAll('.carousel-slide');
-const prevButton = document.getElementById('prevButton');
-const nextButton = document.getElementById('nextButton');
+// const prevButton = document.getElementById('prevButton');
+// const nextButton = document.getElementById('nextButton');
 let currentIndex = 0;
 
 function showSlide(index) {
     slides.forEach((slide, i) => {
         if (i === index) {
-            slide.style.display = 'block';
+            // slide.style.display = 'block';
+            slide.classList.add('ativo');
+            slide.classList.remove('inativo');
         } else {
-            slide.style.display = 'none';
+            // slide.style.display = 'none';
+            slide.classList.remove('ativo');
+            slide.classList.add('inativo');
         }
     });
 }
@@ -27,8 +31,10 @@ function prevSlide() {
     showSlide(currentIndex);
 }
 
-prevButton.addEventListener('click', prevSlide);
-nextButton.addEventListener('click', nextSlide);
+// prevButton.addEventListener('click', prevSlide);
+// nextButton.addEventListener('click', nextSlide);
+document.getElementById('prevButton').addEventListener('click', prevSlide);
+document.getElementById('nextButton').addEventListener('click', nextSlide);
 
 //troca automatica de imagem no carousel
 setInterval(nextSlide, 5000);
